91ff60944344aded6f8d02c68aabc116c2dc7574
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-03-23 16:45:03 +0200

    Add base at the start

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,5 +1,5 @@
 if(location.hostname.includes('github')) {
     const base = document.createElement('base')
-    base.href = '/osm-bulgaria-tools/';
+    base.href = 'https://dimitar5555.github.io/osm-bulgaria-tools/';
     document.head.appendChild(base);
 }
