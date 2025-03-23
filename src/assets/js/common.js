363bc1cdf1f02a3bdde577ae8ada3ac7fe94fcfd
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-03-23 16:50:47 +0200

    Update build, discard base html tag

--- src/assets/js/common.js
+++
@@ -1,5 +0,0 @@
-if(location.hostname.includes('github')) {
-    const base = document.createElement('base')
-    base.href = 'https://dimitar5555.github.io/osm-bulgaria-tools/';
-    document.head.appendChild(base);
-}
